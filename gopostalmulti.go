f5e27b67fec631c9e8ebb25bf41571688c1a28dc
Matt Zimmerman <mzimmerman@gmail.com>
2019-05-06 12:26:57 -0400

    don't use cgo at all, external c command is faster

--- gopostalmulti.go
+++ gopostalmulti.go
@@ -2,13 +2,13 @@ package gopostalmulti
 
 import (
 	"log"
+	"math/rand"
 	"os/exec"
 	"runtime"
 	"sync"
 	"time"
 
 	"github.com/dc0d/tinykv"
-	postal "github.com/openvenues/gopostal/parser"
 	"github.com/vmihailenco/msgpack"
 )
 
@@ -22,27 +22,17 @@ var pool = sync.Pool{
 
 var kv = tinykv.New(time.Minute, nil)
 
-var requestChan = make(chan request, 100)
-
 type request struct {
 	address string
 	resp    chan [][2]string
 }
 
-var count = 0
-
-var mu sync.Mutex
+var workers []chan request
 
 var path = "gopostalmultic"
 
 // start a new underlying OS process of "path" to process locations
 func startOne() {
-	mu.Lock()
-	defer mu.Unlock()
-	if count >= runtime.NumCPU()/2 { // main thread has to produce work and cgo libpostal thread is working too
-		return // can't start any more, not helpful
-	}
-	count++
 	cmd := exec.Command(path)
 	writer, err := cmd.StdinPipe()
 	if err != nil {
@@ -56,44 +46,28 @@ func startOne() {
 	if err != nil {
 		log.Fatalf("Unable to init gopostalmulti - %v", err)
 	}
-
+	reqChan := make(chan request)
+	workers = append(workers, reqChan)
 	go func() {
 		decoder := msgpack.NewDecoder(reader)
-		for {
-			select {
-			case req := <-requestChan:
-				writer.Write([]byte(req.address))
-				writer.Write([]byte{'\n'})
-				out := [][2]string{}
-				err := decoder.Decode(&out)
-				if err != nil {
-					log.Fatalf("Error decoding address - %#v", err)
-				}
-				req.resp <- out
-			case <-time.After(time.Millisecond * 100):
-				writer.Close()
-				reader.Close()
-				cmd.Process.Kill()
-				mu.Lock()
-				count--
-				mu.Unlock()
-				return
+		defer cmd.Wait()
+		for req := range reqChan {
+			writer.Write([]byte(req.address))
+			writer.Write([]byte{'\n'})
+			out := [][2]string{}
+			err := decoder.Decode(&out)
+			if err != nil {
+				log.Fatalf("Error decoding address - %#v", err)
 			}
+			req.resp <- out
 		}
 	}()
 }
 
 func init() { // serve one thread that is "native" through cgo
-	go func() {
-		for req := range requestChan {
-			resp := postal.ParseAddress(req.address)
-			out := make([][2]string, 0, len(resp))
-			for x := range resp {
-				out = append(out, [2]string{resp[x].Label, resp[x].Value})
-			}
-			req.resp <- out
-		}
-	}()
+	for x := 0; x < runtime.NumCPU(); x++ { // create a libpostal processor for each CPU
+		startOne()
+	}
 }
 
 // Parse will parse addresses and return postal components
@@ -106,12 +80,20 @@ func Parse(address string) [][2]string {
 	req := pool.Get().(request)
 	defer pool.Put(req)
 	req.address = address
-	start := time.Now()
-	requestChan <- req
+	sentIt := false
+FindWorker:
+	for x := range workers {
+		select {
+		case workers[x] <- req:
+			sentIt = true
+			break FindWorker // req was accepted!
+		default:
+		}
+	}
+	if !sentIt {
+		workers[rand.Intn(len(workers))] <- req
+	}
 	newresp := <-req.resp
 	kv.Put(address, newresp)
-	if time.Since(start) > time.Millisecond {
-		startOne()
-	}
 	return newresp
 }
